a62ff319b3974cfd96321f720fea03464f623165
RuanPetrus <xastroboyx11@gmail.com>
2023-03-07 16:25:57 -0300

    Add barycentric coordenades

---
+++ test/barycentric.s
@@ -0,0 +1,26 @@
+.text
+  li a0, 0
+  li a1, 0
+  li a2, 0
+  li a3, 319
+  li a4, 239
+  li a5, 160
+  li a6, 50
+  li a7, 50
+
+  call BARYCENTRIC
+  li a7, 2
+  ecall
+  
+  li a0, '\n'
+  li a7, 11
+  ecall
+
+  fmv.s fa0, fa1
+  li a7, 2
+  ecall
+
+  li a7, 10
+  ecall
+
+.include "../src/barycentric.s"
